234c2f00c58ba6f1efeabb3d835dacd676636dfe
wpank <will.pankiewicz@gmail.com>
2017-06-14 16:48:10 -0500

    fixed typo in metadata, expanded block

--- block.py
+++ block.py
@@ -1,7 +1,19 @@
 import hashlib
+from . import BlockMetadata
 
 class Block:
 
     def __init__(self, metadata):
-        
-    def __str__():
+        self.index = metadata.index
+        self.previous_hash = params.previous_hash
+        self.timestamp = params.timestamp
+        self.data = params.data
+        self.hash = self.calc_hash()
+
+    def metadata(self):
+        return BlockMetadata.BlockMetadata(
+            self.index,
+            self.previous_hash,
+            self.timestamp,
+            self.data
+        )
